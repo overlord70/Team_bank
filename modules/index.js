01a2746bead7f606ecb4dee532cfc34b10d29108
aziza <azizabonusaliyeva@gmail.com>
2024-02-27 00:39:47 +0500

    modules
    modules

---
+++ modules/index.js
@@ -0,0 +1,45 @@
+
+
+function createHeader() {
+    let header = document.createElement('header')
+let box = document.createElement('div')
+let box_l = document.createElement('div')
+let box_r = document.createElement('div')
+let glavnaya = document.createElement('a')
+let koshelek = document.createElement('a')
+let akcii = document.createElement('a')
+let email = document.createElement('a')
+let icon = document.createElement('img')
+
+
+
+box.classList.add('box')
+box_l.classList.add('box_l')
+box_r.classList.add('box_r')
+icon.classList.add('icon')
+glavnaya.classList.add('glavnaya')
+koshelek.classList.add('koshelek')
+akcii.classList.add('akcii')
+icon.classList.add('icon')
+
+document.body.appendChild(header)
+header.append(box)
+box.append(box_l, box_r)
+box_l.append(glavnaya, koshelek, akcii)
+box_r.append(email, icon)
+
+
+glavnaya.innerHTML = 'Главная'
+koshelek.innerHTML = 'Мои кошельки'
+akcii.innerHTML = "Мои транзакции"
+email.innerHTML = 'alexadams@google.com'
+icon.src = "./img/log-out (1) 1.png"
+
+icon.onclick  = () => {
+    let btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = 'Выйти'
+    box_r.appendChild(btn);
+    
+}    
+}
