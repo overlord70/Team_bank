db87251dcf8b6862919832f2b13ce85e8597bc24
lecksa <dolabesipa@gmail.com>
2024-03-14 13:02:11 +0500

    added

--- pages/wallets/script.js
+++ pages/wallets/script.js
@@ -5,11 +5,13 @@ let user = JSON.parse(localStorage.getItem('user'))
 createHeader()
 
 let wrap = document.querySelector('.reload')
+let add = document.querySelector('.add')
 
 getData('/wallets?user_id=' + user.id)
     .then(res => {
         if(res.status === 201 || res.status === 200) {
             reload(res.data, wrap)
+            wrap_reload.append(add)
         }
     })
 
